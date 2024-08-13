4e045093d1f5f620adbb58c87cf5fd64fe1bcc38
Denny SM <denny.saviant@gmail.com>
2024-08-13 22:29:00 +0700

    my nvim nvchad config

---
+++ lua/plugins/configs/treesitter.lua
@@ -0,0 +1,11 @@
+local options = {
+  ensure_installed = { "lua", "vim", "vimdoc","blade","php","php_only","html","css","js"},
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+  indent = { enable = true }
+}
+
+return options
